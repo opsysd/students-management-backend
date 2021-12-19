22732fcce35a0addc8305a61803902e501cf6900
opsysd <75551401+opsysd@users.noreply.github.com>
2021-12-19 17:20:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const pool = require('./db_config');
 const cors = require("cors");
 const bodyParser = require('body-parser');
-// const router = require('express').Router();
 
 const app = express();
 
